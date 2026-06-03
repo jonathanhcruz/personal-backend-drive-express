1f1a6bddc8561039cffe2ca1f7f7bdd6ae93ccd7
joncru <jonathan.h.cruz@live.com>
2026-06-02 21:59:31 -0500

    config-db

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 import { errorMiddleware } from './shared/middlewares/error.middleware';
 import authRoutes from './modules/auth/http/auth.routes';
